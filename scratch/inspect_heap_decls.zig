047ea7356c6468ea3e4e06801cdaafb3cb5f06d7
eronom <vishnu.eronom@gmail.com>
2026-05-16 20:49:43 +0530

    rm old code base

--- scratch/inspect_heap_decls.zig
+++
@@ -1,7 +0,0 @@
-const std = @import("std");
-pub fn main() !void {
-    const T = std.heap;
-    inline for (std.meta.declarations(T)) |decl| {
-        std.debug.print("decl: {s}\n", .{decl.name});
-    }
-}
